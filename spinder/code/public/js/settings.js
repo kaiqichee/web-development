db7a419ba33943654f26a0b27b679bd00fd76554
kaiqichee <55299653+kaiqichee@users.noreply.github.com>
2021-10-04 13:01:19 -0400

    Add files via upload

---
+++ spinder/code/public/js/settings.js
@@ -0,0 +1,9 @@
+function OpenForm(){
+    document.getElementById('edit-profile-form').style.display = 'block';
+    document.getElementById('cancel-button').style.display = 'block';
+}
+function CloseForm(){
+    document.getElementById('edit-profile-form').style.display = 'none';
+    document.getElementById('cancel-button').style.display = 'none';
+}
+
