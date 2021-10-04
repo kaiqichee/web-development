e227ff46a875848601d3ffbeb6a1feb30cfcfd6d
kaiqichee <55299653+kaiqichee@users.noreply.github.com>
2021-10-04 12:04:00 -0400

    Add files via upload

---
+++ lab6/app.js
@@ -0,0 +1,11 @@
+//I pledge my honor that I have abided by the Stevens Honor System.
+const express = require("express");
+const app = express();
+const configRoutes = require("./routes");
+
+app.use(express.json());
+configRoutes(app);
+
+app.listen(3000, () => {
+    console.log("Your routes will be running on http://localhost:3000");
+});
